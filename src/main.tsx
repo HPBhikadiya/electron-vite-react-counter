32ffaeb1c2cb7384bcafb3e504a05170eabaacc2
Hiren Bhikadiya <hirenpbhikadiya@gmail.com>
2025-10-31 16:50:46 +0530

    Fix import paths in main.tsx for App and ThemeContextProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
-import { ThemeContextProvider } from './contexts/ThemeContext';
+import App from './App';
+import { ThemeContextProvider } from './contexts/ThemeContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
